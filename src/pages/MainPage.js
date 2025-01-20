97015a36ff22c88af0bffdb3fec6906c6d94fda4
zzaming <whaudgus123@naver.com>
2025-01-20 15:54:14 +0900

    first commit

---
+++ src/pages/MainPage.js
@@ -0,0 +1,13 @@
+// import { Link } from "react-router-dom";
+import BasicLayout from "../layouts/BasicLayout";
+
+const MainPage = () => {
+    return (
+        <BasicLayout>
+            {/* <div className="flex"><Link to={"/about"}>About</Link></div> */}
+            <div className="text-3xl">Main Page</div>
+        </BasicLayout>
+    )
+}
+
+export default MainPage;
