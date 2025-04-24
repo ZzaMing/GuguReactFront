1f3b6ac59f301a4ddaa9e200508238caf3c50a3b
zzaming <whaudgus123@naver.com>
2025-04-24 17:22:06 +0900

    4/24

--- src/components/products/ReadComponent.js
+++ src/components/products/ReadComponent.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { API_SERVER_HOST, putOne } from "../../api/todoApi"
+import { API_SERVER_HOST,} from "../../api/todoApi"
 import useCustomMove from "../../hooks/useCustomMove"
 import { getOne } from "../../api/productsApi"
 import FetchingModal from "../common/FetchingModal"
@@ -46,11 +46,64 @@ const ReadComponent = ({ pno }) => {
                     <div className="w-4/5 p-6 rounded-r border border-solid shadow-md">
                         {product.pno}
                     </div>
-
                 </div>
-
             </div>
-
+            <div className="flex justify-center">
+                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                    <div className="w-1/5 p-6 text-right font-bold">
+                        PNAME
+                    </div>
+                    <div className="w-4/5 p-6 rounded-r border border-solid shadow-md">
+                        {product.pname}
+                    </div>
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                    <div className="w-1/5 p-6 text-right font-bold">
+                        PRICE
+                    </div>
+                    <div className="w-4/5 p-6 rounded-r border border-solid shadow-md">
+                        {product.price}
+                    </div>
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                    <div className="w-1/5 p-6 text-right font-bold">
+                        PDESC
+                    </div>
+                    <div className="w-4/5 p-6 rounded-r border border-solid shadow-md">
+                        {product.pdesc}
+                    </div>
+                </div>
+            </div>
+            <div className="w-full justify-center flex flex-col m-auto items-center">
+                {product.uploadFileNames.map((imgFile, i) => 
+                    <img
+                        alt="product"
+                        key={i}
+                        className="p-4 w-1/2"
+                        src={`${host}/api/products/view/${imgFile}`}
+                    />
+                )}
+            </div>
+            <div className="flex justify-end p-4">
+                <button
+                    type="button"
+                    className="inline-block rounded p-4 m-2 textl-xl w-32 text-white bg-blue-500"
+                    onClick={() => moveToModify(pno)}
+                >
+                    Modify
+                </button>
+                <button
+                    type="button"
+                    className="inline-block rounded p-4 m-2 textl-xl w-32 text-white bg-red-500"
+                    onClick={moveToList}
+                >
+                    List
+                </button>
+            </div>
 
         </div>
     )
--- src/router/productsRouter.js
+++ src/router/productsRouter.js
@@ -5,6 +5,7 @@ const Loading = <div>Loading..........</div>
 const ProductsList = lazy(() => import("../pages/products/ListPage"))
 const ProductsAdd = lazy(() => import("../pages/products/AddPage"))
 const ProductsRead = lazy(() => import("../pages/products/ReadPage"))
+const ProductsModify = lazy(() => import("../pages/products/ModifyPage"))
 
 const productRouter = () => {
 
@@ -24,6 +25,10 @@ const productRouter = () => {
         {
             path: "read/:pno",
             element: <Suspense fallback={Loading}><ProductsRead/></Suspense>
+        },
+        {
+            path: "modify/:pno",
+            element: <Suspense fallback={Loading}><ProductsModify/></Suspense>
         }
     ]
 
